cfb7ee6a41ff78227eacb5af3c4a39d6ba85289f
andyjimenez956 <123230241+andyjimenez956@users.noreply.github.com>
2023-04-06 13:01:31 -0500

    Add project files.

---
+++ GamesApp/GamesApp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+
+highLowIntro();
+highLowGameplay();
+highLowPostGameOptions();
+
+int main()
+{
+
+    highLowIntro();
+    highLowGameplay();
+    highLowPostGameOptions();
+
+}
+
+void highLowIntro() {
+    std::cout << "This game is called High Low. \n";
+    std::cout << "The Object of the game is to get if the next number is \n";
+    std::cout << "higher or lower than the current number on display.\n";
+    std::cout << "It is a game of alot of luck but also probability.\n";
+    std::cout << "When you have your guess type in (h/H) for high or (l/L) for low.\n";
+    std::cout << "Then click enter. \n";
+    std::cout << "Try and get the highest score you can. Lets Start! \n";
+}
+
+void highLowGameplay() {
+
+}
+
+void highLowPostGameOptions() {
+
+}
